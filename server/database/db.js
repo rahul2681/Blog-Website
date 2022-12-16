711a3268bec7775896fec252739a484fec5e56f2
rahul2681 <80594958+rahul2681@users.noreply.github.com>
2022-12-16 17:48:42 +0530

    Add files via upload

---
+++ server/database/db.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const Connection = async (username, password) => {
+    const URL = `mongodb+srv://abc:abc123456789@cluster0.aaku2lr.mongodb.net/?retryWrites=true&w=majority
+    `;
+    try {
+        await mongoose.connect(URL, { useNewUrlParser: true })
+        console.log('Database connected successfully');
+    } catch (error) {
+        console.log('Error while connecting to the database ', error);
+    }
+};
+
+export default Connection;
